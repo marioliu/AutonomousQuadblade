749206c19fd485eeece24decd43632951716b8a9
Nadir Noordin <nadirn.noordin27@gmail.com>
2019-04-05 12:37:17 -0400

    updated simple.py to run a function that outputs coordinates

--- Drone_Control/simple1.py
+++ Drone_Control/simple1.py
@@ -1,3 +1,10 @@
+# Author: Nadir Noordin
+
+# Description: a function that gets coordinates from the piksi gps
+# Adapted from simple.py provided by Swift Navigation 
+
+
+
 # Copyright (C) 2015 Swift Navigation Inc.
 # Contact: Fergus Noble <fergus@swiftnav.com>
 #
@@ -13,11 +20,44 @@ reading SBP messages from a serial port, decoding BASELINE_NED messages and
 printing them out.
 """
 
+
 from sbp.client.drivers.pyserial_driver import PySerialDriver
 from sbp.client import Handler, Framer
 from sbp.client.loggers.json_logger import JSONLogger
 from sbp.navigation import SBP_MSG_BASELINE_NED, MsgBaselineNED
 import argparse
+import time
+
+
+
+portNum = 0
+
+
+def getData(portNum):
+
+    directory = '/dev/ttyUSB' + str(portNum)
+    north = None
+    east = None
+    down = None
+    # Open a connection to Piksi using the default baud rate (1Mbaud)
+    with PySerialDriver(directory, baud=1000000) as driver:
+        with Handler(Framer(driver.read, None )) as source:
+            try:
+                for msg, metadata in source.filter(SBP_MSG_BASELINE_NED):
+                    north = msg.n * 1e-3
+                    east = msg.e * 1e-3
+                    down = msg.d * 1e-3
+
+
+                    if north != None and east != None and down != None:
+                        # Print out the N, E, D coordinates of the baseline
+                        print("%.4f,%.4f,%.4f" % (msg.n * 1e-3, msg.e * 1e-3,
+                                              msg.d * 1e-3))
+                        return north,east,down
+
+            except KeyboardInterrupt:
+                pass
+
 
 def main():
     parser = argparse.ArgumentParser(
@@ -25,21 +65,16 @@ def main():
     parser.add_argument(
         "-p",
         "--port",
-        default=['/dev/ttyUSB2'],
+        default=['/dev/ttyUSB0'],
         nargs=1,
         help="specify the serial port to use.")
     args = parser.parse_args()
 
-    # Open a connection to Piksi using the default baud rate (1Mbaud)
-    with PySerialDriver(args.port[0], baud=1000000) as driver:
-        with Handler(Framer(driver.read, None )) as source:
-            try:
-                for msg, metadata in source.filter(SBP_MSG_BASELINE_NED):
-                    # Print out the N, E, D coordinates of the baseline
-                    print("%.4f,%.4f,%.4f" % (msg.n * 1e-3, msg.e * 1e-3,
-                                              msg.d * 1e-3))
-            except KeyboardInterrupt:
-                pass
+    # run the function to get data from the gps
+    getData(portNum)    
+
+
 
 if __name__ == "__main__":
     main()
+
