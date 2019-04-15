3ec60df9090232442df7a508df8ae074ba0ba2af
Mario Liu <marioliu@princeton.edu>
2019-04-15 16:31:21 -0400

    Reorganized sample data, started working on thesis.py

--- thesis.py
+++ thesis.py
@@ -9,8 +9,7 @@ from Algorithms import adaptive_grid_sizing as ags
 from Algorithms import rbf_interpolation as rbfi
 from Algorithms import nav
 from process_frames import plot2
-from Drone_Control import mission_move_drone as md
-from Drone_Control import piksi
+from process_frames import getFramesFromSource
 
 import matplotlib.pyplot as plt
 import time
@@ -19,8 +18,68 @@ import numpy as np
 from dronekit import connect
 from pymavlink import mavutil
 
+def dist_test():
+    source = './Camera/Sample_Data/dist_comparison/25'
+    d_25, c_25 = getFramesFromSource(source)
+
+    source = './Camera/Sample_Data/dist_comparison/26'
+    d_26, c_26 = getFramesFromSource(source)
+    # figsize = width, height
+    # figsize = (4.5, 5.5)
+
+    fig = plt.figure()
+    plt.subplot(2, 2, 1)
+    plt.imshow(c_25)
+    plt.title('Color Image at 25 Inches')
+    plt.grid()
+
+    plt.subplot(2, 2, 2)
+    plt.imshow(d_25, cmap='gist_rainbow')
+    plt.colorbar()
+    plt.title('Depth Image at 25 Inches', y=1.04)
+    plt.grid()
+
+    plt.subplot(2, 2, 3)
+    plt.imshow(c_26)
+    plt.title('Color Image at 26 Inches')
+    plt.grid()
+
+    plt.subplot(2, 2, 4)
+    plt.imshow(d_26, cmap='gist_rainbow')
+    plt.colorbar()
+    plt.title('Depth Image at 26 Inches', y=1.04)
+    plt.grid()
+
+    plt.subplots_adjust(hspace = 0.4)
+
+    plt.show()
+
+def outdoors():
+    source = './Camera/Sample_Data/center_tree'
+    d, c = getFramesFromSource(source)
+
+    # figsize = width, height
+    figsize = (6, 2.5)
+
+    fig = plt.figure(figsize = figsize)
+    plt.subplot(1, 2, 1)
+    plt.imshow(c)
+    plt.title('Color Image')
+    plt.grid()
+
+    plt.subplot(1, 2, 2)
+    plt.imshow(d, cmap='gist_rainbow')
+    plt.colorbar(fraction = 0.046, pad = 0.04)
+    plt.title('Depth Image')
+    plt.grid()
+
+    plt.subplots_adjust(hspace = 0.4)
+
+    plt.show()
+
 def main():
-    pass
+    # dist_test()
+    outdoors()
 
 if __name__ == "__main__":
     try:
